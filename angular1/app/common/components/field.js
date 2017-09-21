47521a8a0a96b02e9560a4046925bc645f41ebe1
visitante <evert90@gmail.com>
2017-09-20 23:59:32 -0300

    Seção 13 - Aula 93

--- angular1/app/common/components/field.js
+++ angular1/app/common/components/field.js
@@ -5,7 +5,8 @@
       grid: '@',
       placeholder: '@',
       type: '@',
-      model: '='
+      model: '=',
+      readonly: '<'
     },
     controller: [
       'gridSystem',
@@ -18,7 +19,8 @@
           <div class="{{ $ctrl.gridClasses }}">
               <div class="form-group">
                 <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
-                <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" type="{{ $ctrl.type }}" ng-model="$ctrl.model" />
+                <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}"
+                type="{{ $ctrl.type }}" ng-model="$ctrl.model" ng-readonly="$ctrl.readonly" />
               </div>
           </div>
     `
