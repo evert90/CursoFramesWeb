59d7d3c240e393a9b0aa433799447e08eca460e7
visitante <evert90@gmail.com>
2017-09-24 20:17:38 -0300

    Curso finalizado (Seção 18 - Aula 121)

--- angular1/app/config/routes.js
+++ angular1/app/config/routes.js
@@ -1,16 +1,49 @@
 angular.module('primeiraApp').config([
   '$stateProvider',
   '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider){
+  '$httpProvider',
+  function($stateProvider, $urlRouterProvider, $httpProvider){
     $stateProvider.state('dashboard', {
       url: "/dashboard",
       templateUrl: "dashboard/dashboard.html"
     }).state('billingCycle', {
-      url: "/billingCycles",
+      url: "/billingCycles?page",
       templateUrl: "billingCycle/tabs.html"
     })
+    
+    $httpProvider.interceptors.push('handleResponseError')
+  }])
+.run([
+  '$rootScope',
+  '$http',
+  '$location',
+  '$window',
+  'auth',
+  function ($rootScope, $http, $location, $window, auth) {
+    validateUser()
 
-    $urlRouterProvider.otherwise('/dashboard')
+    $rootScope.$on('$locationChangeStart', () => validateUser())
+
+    function validateUser() {
+      const user = auth.getUser()
+      const authPage = '/auth.html'
+      const isAuthPage = $window.location.href.includes(authPage)
+
+
+      if (!user && !isAuthPage) {
+        $window.location.href = authPage
+      } else if (user && !user.isValid) {
+        auth.validateToken(user.token, (err, valid) => {
+          if (!valid) {
+            $window.location.href = authPage
+          } else {
+            user.isValid = true
+            $http.defaults.headers.common.Authorization = user.token
+            isAuthPage ? $window.location.href = '/' : $location.path('/dashboard')
+          }
+        })
+      }
+    }
 
   }
 ])
